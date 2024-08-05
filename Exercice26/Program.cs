1919772581485548addc0296ebf47d869c990cdc
merouane <merouane@PC-MRW>
2024-08-05 16:58:25 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice26/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercice26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclarations
+            int saisie, heure = 0, minute, seconde;
+
+            //Sasisie d'un nombre de secondes
+            do
+            {
+                Console.Write("Saisissez un nombre de secondes(entre 0 et 86400) : ");
+                saisie = int.Parse(Console.ReadLine());
+            } while (saisie < 0 || saisie > 86400);
+            
+
+            //Conversion
+            heure = saisie / 3600;
+            minute = (saisie - (heure * 3600)) / 60;
+            seconde = saisie - (heure * 3600) - (minute*60);
+
+            //Formatage de l'affcichage
+            string affichage = "";
+            if (heure<10)
+            {
+                affichage += "0";
+            }
+            affichage += heure + ":";
+
+            if (minute < 10)
+            {
+                affichage += "0";
+            }
+            affichage += minute + ":";
+
+            if (seconde < 10)
+            {
+                affichage += "0";
+            }
+            affichage += seconde ;
+
+            //Affichage final sous forme HH:MM:SS
+
+            Console.Write("Voici la conversion du nombre de secondes : " + affichage);
+            Console.ReadLine();
+
+
+        }
+    }
+    //Correction: Au lieu de tronquer ecrire en int directement
+}
